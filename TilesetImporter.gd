137340925b289d1dc1cd92dfd1b4dd08102502f5
RustyRoboticsBV <rusty.robotics.bv@gmail.com>
2025-08-26 15:24:58 +0200

    Added initial version that can load edge tiles.

---
+++ TilesetImporter.gd
@@ -0,0 +1,43 @@
+@tool
+extends EditorImportPlugin;
+
+func _get_importer_name() -> String:
+	return "tileset_maker";
+	
+func _get_import_options(String, int) -> Array[Dictionary]:
+	return [];
+	
+func _get_visible_name() -> String:
+	return "Tileset Importer";
+	
+func _get_recognized_extensions() -> PackedStringArray:
+	return ["zip"];
+	
+func _get_preset_name(_index : int) -> String:
+	return "Default";
+	
+func _get_resource_type() -> String:
+	return "Texture2D";
+	
+func _get_priority() -> float:
+	return 1.0;
+	
+func _get_save_extension() -> String:
+	return "res";
+	
+func _import(source_file: String, save_path: String, _options: Dictionary, _platform_variants: Array, _gen_files: Array) -> int:
+	print("Importing tileset from: '%s'" % source_file);
+
+	# Create tileset.
+	var tileset_maker = TilesetMaker.new();
+	var tileset : Texture2D = tileset_maker.create_tileset(source_file);
+	
+	# Save the resulting resource.
+	var save_file = "%s.%s" % [save_path, _get_save_extension()];
+	var err = ResourceSaver.save(tileset, save_file);
+
+	if err != OK:
+		push_error("Failed to save imported tileset: %s" % save_file)
+		return err;
+
+	return OK;
---
+++ TilesetMaker.gd
@@ -0,0 +1,392 @@
+@tool
+class_name TilesetMaker;
+extends Node;
+
+# The IDs of all tiles.
+enum TileID {
+	EDGE_L,
+	EDGE_R,
+	EDGE_B,
+	EDGE_T,
+	NOOK_TL,
+	NOOK_TR,
+	NOOK_BL,
+	NOOK_BR,
+	CENTER,
+	CORNER_TL,
+	CORNER_TR,
+	CORNER_BL,
+	CORNER_BR,
+	CAP_T,
+	CAP_B,
+	CAP_L,
+	CAP_R,
+	MIDDLE_H,
+	MIDDLE_V,
+	SMALL,
+	GAP_L,
+	GAP_R,
+	GAP_B,
+	GAP_T,
+	DIAG_D,
+	DIAG_U,
+	HUB_TL,
+	HUB_TR,
+	HUB_BL,
+	HUB_BR,
+	CROSS,
+	TURN_TL,
+	TURN_TR,
+	TURN_BL,
+	TURN_BR,
+	JUNCTION_L,
+	JUNCTION_R,
+	JUNCTION_B,
+	JUNCTION_T,
+	EXIT_TL_H,
+	EXIT_TL_V,
+	EXIT_TR_H,
+	EXIT_TR_V,
+	EXIT_BL_H,
+	EXIT_BL_V,
+	EXIT_BR_H,
+	EXIT_BR_V
+}
+
+class TileImage:
+	var id : TileID;
+	var image : Image;
+	
+	# Return the width of the image.
+	func get_width() -> int:
+		return image.get_width();
+	
+	# Return the height of the image.
+	func get_height() -> int:
+		return image.get_height();
+	
+	# Return a duplicate of this tile image.
+	func copy() -> TileImage:
+		var mycopy : TileImage = TileImage.new();
+		mycopy.id = id;
+		mycopy.image = image.duplicate();
+		return mycopy;
+	
+	# Return a duplicate of this tile image that has been flipped horizontally.
+	func flip_x() -> TileImage:
+		var mycopy = copy();
+		mycopy.image.flip_x();
+		return mycopy;
+	
+	# Return a duplicate of this tile image that has been flipped vertically.
+	func flip_y() -> TileImage:
+		var mycopy = copy();
+		mycopy.image.flip_y();
+		return mycopy;
+	
+	# Return a duplicate of this tile image that has been rotated clockwise.
+	func rotate_clock() -> TileImage:
+		var mycopy = copy();
+		mycopy.image.rotate_90(CLOCKWISE);
+		return mycopy;
+	
+	# Return a duplicate of this tile image that has been rotated counter-clockwise.
+	func rotate_counter() -> TileImage:
+		var mycopy = copy();
+		mycopy.image.rotate_90(COUNTERCLOCKWISE);
+		return mycopy;
+	
+	# Create an image from the left half of one image and the right half of another.
+	func combine_h(img_left: TileImage, img_right: TileImage):
+		# Ensure both images are the same size
+		if img_left.get_width() != img_right.get_width() or img_left.get_height() != img_right.get_height():
+			push_error("Images must have the same dimensions!")
+			return null;
+		
+		var width : int = img_left.get_width();
+		var height : int = img_left.get_height();
+		var half_width : int = width / 2;
+		
+		# Create a new empty image with the same size
+		image = Image.create(width, height, false, img_left.get_format());
+		
+		# Copy the left half of img_left
+		image.blit_rect(img_left.image, Rect2(Vector2.ZERO, Vector2(half_width, height)), Vector2.ZERO);
+		
+		# Copy the right half of img_right
+		image.blit_rect(img_right.image, Rect2(Vector2(half_width, 0), Vector2(half_width, height)), Vector2(half_width, 0));
+	
+	# Blit this tile image onto a larger image.
+	func blit_onto(dst_image : Image, tile_x : int, tile_y : int, tile_w : int, tile_h : int):
+		# Do nothing if we didn't resolve.
+		if image == null:
+			return;
+		print("Blitting " + TileID.keys()[id]);
+		
+		# Match the target format.
+		if image.get_format() != dst_image.get_format():
+			image.convert(dst_image.get_format());
+		
+		# Error if our width and height don't match the larger image's tile width and height.
+		if get_width() != tile_w or get_height() != tile_h:
+			push_warning("The tile %s's size did not match the tileset's tilesize.", id);
+			
+		# Define the source rectangle.
+		var src_rect : Rect2 = Rect2(0, 0, get_width(), get_height());
+		
+		# Blit onto the destination at the same position.
+		dst_image.blit_rect(image, src_rect, Vector2(tile_x * tile_w, tile_y * tile_h));
+
+
+var src_images : Dictionary[String, Image] = {};
+var dst_images : Array[TileImage] = [];
+
+# Load a tileset texture from a ZIP file.
+func create_tileset(file_path : String) -> Texture2D:
+	# Load images from ZIP file.
+	src_images = load_images_from_zip(file_path);
+	print("Images in ZIP: " + str(src_images));
+	
+	# Create list of empty tile images.
+	dst_images = [];
+	for id in TileID.values():
+		var tile_image : TileImage = TileImage.new();
+		tile_image.id = id;
+		dst_images.append(tile_image);
+	
+	# Try to resolve all tiles.
+	print();
+	print("Resolving tiles...")
+	var loop_index : int = 0;
+	while true:
+		print("Loop " + str(loop_index) + ": ");
+		var changed : bool = false;
+		for id in TileID:
+			if resolve_edge_l():
+				print("Resolved EDGE_L");
+				changed = true;
+			if resolve_edge_r():
+				print("Resolved EDGE_R");
+				changed = true;
+			if resolve_edge_t():
+				print("Resolved EDGE_T");
+				changed = true;
+			if resolve_edge_b():
+				print("Resolved EDGE_B");
+				changed = true;
+		if not changed:
+			print("No more tiles were resolved.");
+			break;
+		
+		loop_index += 1;
+		if loop_index == 1000:
+			push_error("Infinite loop detected!");
+			break;
+	
+	# Figure out tile size.
+	print();
+	print("Determining tile size...");
+	var tile_w : int = 0;
+	var tile_h : int = 0;
+	for id in TileID.values():
+		if has_resolved(id):
+			var my_tile_w : int = get_resolved(id).get_width();
+			if my_tile_w > tile_w:
+				tile_w = my_tile_w;
+			var my_tile_h : int = get_resolved(id).get_height();
+			if my_tile_h > tile_h:
+				tile_h = my_tile_h;
+	
+	if tile_w == 0 or tile_h == 0:
+		var empty_image : Image = Image.create(1, 1, false, Image.FORMAT_RGBA8);
+		return ImageTexture.create_from_image(empty_image);
+	print("Tile size = (" + str(tile_w) + ", " + str(tile_h) + ")");
+	
+	# Copy all resolved tiles to a texture.
+	print();
+	print("Building tileset...");
+	var image : Image = Image.create(tile_w * 12, tile_h * 4, false, Image.FORMAT_RGBA8);
+	get_resolved(TileID.CAP_T).blit_onto(image, 0, 0, tile_w, tile_h);
+	get_resolved(TileID.TURN_TL).blit_onto(image, 1, 0, tile_w, tile_h);
+	get_resolved(TileID.JUNCTION_T).blit_onto(image, 2, 0, tile_w, tile_h);
+	get_resolved(TileID.TURN_TR).blit_onto(image, 3, 0, tile_w, tile_h);
+	get_resolved(TileID.HUB_BR).blit_onto(image, 4, 0, tile_w, tile_h);
+	get_resolved(TileID.EXIT_TL_H).blit_onto(image, 5, 0, tile_w, tile_h);
+	get_resolved(TileID.EXIT_TR_H).blit_onto(image, 6, 0, tile_w, tile_h);
+	get_resolved(TileID.HUB_BL).blit_onto(image, 7, 0, tile_w, tile_h);
+	get_resolved(TileID.NOOK_TL).blit_onto(image, 8, 0, tile_w, tile_h);
+	get_resolved(TileID.GAP_T).blit_onto(image, 9, 0, tile_w, tile_h);
+	get_resolved(TileID.EDGE_T).blit_onto(image, 10, 0, tile_w, tile_h);
+	get_resolved(TileID.NOOK_TR).blit_onto(image, 11, 0, tile_w, tile_h);
+	
+	get_resolved(TileID.MIDDLE_V).blit_onto(image, 0, 1, tile_w, tile_h);
+	get_resolved(TileID.JUNCTION_L).blit_onto(image, 1, 1, tile_w, tile_h);
+	get_resolved(TileID.CROSS).blit_onto(image, 2, 1, tile_w, tile_h);
+	get_resolved(TileID.JUNCTION_R).blit_onto(image, 3, 1, tile_w, tile_h);
+	get_resolved(TileID.EXIT_TL_V).blit_onto(image, 4, 1, tile_w, tile_h);
+	get_resolved(TileID.CORNER_TL).blit_onto(image, 5, 1, tile_w, tile_h);
+	get_resolved(TileID.CORNER_TR).blit_onto(image, 6, 1, tile_w, tile_h);
+	get_resolved(TileID.EXIT_TR_V).blit_onto(image, 7, 1, tile_w, tile_h);
+	get_resolved(TileID.EDGE_L).blit_onto(image, 8, 1, tile_w, tile_h);
+	get_resolved(TileID.DIAG_U).blit_onto(image, 9, 1, tile_w, tile_h);
+	#get_resolved(TileID.EMPTY).blit_onto(image, 10, 1, tile_w, tile_h);
+	get_resolved(TileID.GAP_R).blit_onto(image, 11, 1, tile_w, tile_h);
+	
+	get_resolved(TileID.CAP_B).blit_onto(image, 0, 2, tile_w, tile_h);
+	get_resolved(TileID.TURN_BL).blit_onto(image, 1, 2, tile_w, tile_h);
+	get_resolved(TileID.JUNCTION_B).blit_onto(image, 2, 2, tile_w, tile_h);
+	get_resolved(TileID.TURN_BR).blit_onto(image, 3, 2, tile_w, tile_h);
+	get_resolved(TileID.EXIT_BL_V).blit_onto(image, 4, 2, tile_w, tile_h);
+	get_resolved(TileID.CORNER_BL).blit_onto(image, 5, 2, tile_w, tile_h);
+	get_resolved(TileID.CORNER_BR).blit_onto(image, 6, 2, tile_w, tile_h);
+	get_resolved(TileID.EXIT_BR_V).blit_onto(image, 7, 2, tile_w, tile_h);
+	get_resolved(TileID.GAP_L).blit_onto(image, 8, 2, tile_w, tile_h);
+	get_resolved(TileID.CENTER).blit_onto(image, 9, 2, tile_w, tile_h);
+	get_resolved(TileID.DIAG_D).blit_onto(image, 10, 2, tile_w, tile_h);
+	get_resolved(TileID.EDGE_R).blit_onto(image, 11, 2, tile_w, tile_h);
+	
+	get_resolved(TileID.SMALL).blit_onto(image, 0, 3, tile_w, tile_h);
+	get_resolved(TileID.CAP_L).blit_onto(image, 1, 3, tile_w, tile_h);
+	get_resolved(TileID.MIDDLE_H).blit_onto(image, 2, 3, tile_w, tile_h);
+	get_resolved(TileID.CAP_R).blit_onto(image, 3, 3, tile_w, tile_h);
+	get_resolved(TileID.HUB_TR).blit_onto(image, 4, 3, tile_w, tile_h);
+	get_resolved(TileID.EXIT_BL_H).blit_onto(image, 5, 3, tile_w, tile_h);
+	get_resolved(TileID.EXIT_BR_H).blit_onto(image, 6, 3, tile_w, tile_h);
+	get_resolved(TileID.HUB_TL).blit_onto(image, 7, 3, tile_w, tile_h);
+	get_resolved(TileID.NOOK_BL).blit_onto(image, 8, 3, tile_w, tile_h);
+	get_resolved(TileID.EDGE_B).blit_onto(image, 9, 3, tile_w, tile_h);
+	get_resolved(TileID.GAP_B).blit_onto(image, 10, 3, tile_w, tile_h);
+	get_resolved(TileID.NOOK_BR).blit_onto(image, 11, 3, tile_w, tile_h);
+	
+	return ImageTexture.create_from_image(image);
+
+# Load all the PNGs from a ZIP file and return them as a dictionary.
+static func load_images_from_zip(path: String) -> Dictionary[String, Image]:
+	var images : Dictionary[String, Image] = {};
+	var zip := ZIPReader.new();
+	var err = zip.open(path);
+	if err != OK:
+		push_error("Failed to open zip file: '%s'" % path);
+		return images;
+	
+	for file_name in zip.get_files():
+		if file_name.to_lower().ends_with(".png"):
+			var bytes = zip.read_file(file_name);
+			if bytes.size() > 0:
+				var img := Image.new();
+				var load_err = img.load_png_from_buffer(bytes);
+				if load_err == OK:
+					var key = file_name.get_basename().replace(" ", "_").to_upper();
+					images[key] = img;
+				else:
+					push_warning("Failed to load PNG from: '%s'" % file_name);
+		elif file_name.to_lower().ends_with(".bmp"):
+			var bytes = zip.read_file(file_name);
+			if bytes.size() > 0:
+				var img := Image.new();
+				var load_err = img.load_bmp_from_buffer(bytes);
+				if load_err == OK:
+					var key = file_name.get_basename().replace(" ", "_").to_upper();
+					images[key] = img;
+				else:
+					push_warning("Failed to load BMP from: '%s'" % file_name);
+	
+	zip.close();
+	return images;
+
+# Try to resolve the EDGE_L tile.
+func resolve_edge_l() -> bool:
+	# Do nothing if already resolved.
+	if has_resolved(TileID.EDGE_L):
+		return false;
+	
+	# Try simple resolve first.
+	if simple_resolve(TileID.EDGE_L):
+		return true;
+	
+	# Load by flipping EDGE_R if present.
+	if has_resolved(TileID.EDGE_R):
+		resolve(TileID.EDGE_L, get_resolved(TileID.EDGE_R).flip_x());
+		return true;
+	
+	# Load by rotating EDGE_T if present.
+	if has_resolved(TileID.EDGE_T):
+		resolve(TileID.EDGE_L, get_resolved(TileID.EDGE_T).rotate_counter());
+		return true;
+	
+	return false;
+
+# Try to resolve the EDGE_R tile.
+func resolve_edge_r() -> bool:
+	# Do nothing if already resolved.
+	if has_resolved(TileID.EDGE_R):
+		return false;
+	
+	# Try simple resolve first.
+	if simple_resolve(TileID.EDGE_R):
+		return true;
+	
+	# Load by flipping EDGE_L if present.
+	if has_resolved(TileID.EDGE_L):
+		resolve(TileID.EDGE_R, get_resolved(TileID.EDGE_L).flip_x());
+		return true;
+	
+	return false;
+	
+# Try to resolve the EDGE_T tile.
+func resolve_edge_t() -> bool:
+	# Do nothing if already resolved.
+	if has_resolved(TileID.EDGE_T):
+		return false;
+	
+	# Try simple resolve first.
+	if simple_resolve(TileID.EDGE_T):
+		return true;
+	
+	# Load by flipping EDGE_B if present.
+	if has_resolved(TileID.EDGE_B):
+		resolve(TileID.EDGE_T, get_resolved(TileID.EDGE_B).flip_x());
+		return true;
+	
+	# Load by rotating EDGE_L if present.
+	if has_resolved(TileID.EDGE_L):
+		resolve(TileID.EDGE_T, get_resolved(TileID.EDGE_L).rotate_clock());
+	
+	return false;
+
+# Try to resolve the EDGE_B tile.
+func resolve_edge_b() -> bool:
+	# Do nothing if already resolved.
+	if has_resolved(TileID.EDGE_B):
+		return false;
+	
+	# Try simple resolve first.
+	if simple_resolve(TileID.EDGE_B):
+		return true;
+	
+	# Load by flipping EDGE_T if present.
+	if has_resolved(TileID.EDGE_T):
+		resolve(TileID.EDGE_B, get_resolved(TileID.EDGE_T).flip_y());
+		return true;
+	
+	return false;
+
+# Try to resolve a tile by directly taking its image from the src_images dictionary.
+func simple_resolve(tile_id : TileID) -> bool:
+	var key : String = TileID.keys()[tile_id];
+	if src_images.has(key):
+		dst_images[tile_id].image = src_images[key];
+		return true;
+	else:
+		return false;
+
+# Resolve a tile using some image.
+func resolve(tile_id : TileID, tileImage : TileImage):
+	dst_images[tile_id].image = tileImage.image;
+
+# Check if we've already resolved some tile.
+func has_resolved(tile_id : TileID) -> bool:
+	return dst_images[tile_id].image != null;
+
+func get_resolved(tile_id : TileID) -> TileImage:
+	return dst_images[tile_id];
