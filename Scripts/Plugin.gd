b9d5ee765b59ad958d316a6d35c840a830887a42
RustyRoboticsBV <rusty.robotics.bv@gmail.com>
2025-09-08 23:16:07 +0200

    Renamed exporter folder.

--- Scripts/Plugin.gd
+++ Scripts/Plugin.gd
@@ -8,7 +8,7 @@ func _enter_tree():
 	import_plugin = preload("TextureImporter/ImportPlugin.gd").new();
 	add_import_plugin(import_plugin);
 	
-	inspector_plugin = preload("TextureExporter/InspectorPlugin.gd").new();
+	inspector_plugin = preload("TextureInspector/InspectorPlugin.gd").new();
 	add_inspector_plugin(inspector_plugin);
 
 func _exit_tree():
